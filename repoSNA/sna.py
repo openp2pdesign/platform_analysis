a9f147d6d2209a6cc9580ae7dee491b03198c4fa
Massimo Menichinelli <info@openp2pdesign.org>
2016-05-17 23:55:34 +0200

    Add an almost empty sna.py for the main sna analysis

---
+++ repoSNA/sna.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+#
+# Social Network Analysis of Git, Hg, SVN, GitHub, BitBucket repositories
+#
+# Author: Massimo Menichinelli
+# Homepage: http://www.openp2pdesign.org
+# License: LGPL v.3
+#
+
+
+import networkx as nx
+
+
+# SNA on a dictionary
+def convert_log_to_dict(input_text):
+    graph = nx.DiGraph()
+    return graph
+
+
+if __name__ == "__main__":
+    pass
