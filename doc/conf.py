484af5ba4d96cea34193887aeac75455c48f803f
Massimo Menichinelli <info@openp2pdesign.org>
2022-06-22 00:12:50 +0200

    Add again sphinx extensions to docs

--- doc/conf.py
+++ doc/conf.py
@@ -30,8 +30,12 @@ release = '0.21'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
